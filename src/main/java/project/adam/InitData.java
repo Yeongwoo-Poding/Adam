340b5247f367cbf950925a2b1c756432daa38838
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-09-13 20:47:12 +0900

    [Fix] Add test profile at InitData

--- src/main/java/project/adam/InitData.java
+++ src/main/java/project/adam/InitData.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 
-@Profile({"local", "dev"})
+@Profile({"local", "dev", "test"})
 @Component
 @RequiredArgsConstructor
 public class InitData {
