c65a8a7b9f9f6bdb5393379a79fdc0c711f262ac
Yeongwoo-Poding <yw1221@g.skku.edu>
2022-09-20 02:11:33 +0900

    [Feature] #204 : Create dummy data

---
+++ src/main/java/project/adam/Init.java
@@ -0,0 +1,78 @@
+package project.adam;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import project.adam.entity.comment.Comment;
+import project.adam.entity.member.Member;
+import project.adam.entity.post.Board;
+import project.adam.entity.post.Post;
+import project.adam.entity.reply.Reply;
+import project.adam.repository.comment.CommentRepository;
+import project.adam.repository.member.MemberRepository;
+import project.adam.repository.post.PostRepository;
+import project.adam.repository.reply.ReplyRepository;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.util.UUID;
+
+@Slf4j
+@Profile({"local", "dev"})
+@Component
+@RequiredArgsConstructor
+public class Init {
+
+    private final Dev dev;
+
+    @PostConstruct
+    public void postConstruct() {
+        dev.createDummyData();
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    private static class Dev {
+
+        private final MemberRepository memberRepository;
+        private final PostRepository postRepository;
+        private final CommentRepository commentRepository;
+        private final ReplyRepository replyRepository;
+
+        private static final long N = 5L;
+
+        @Transactional
+        public void createDummyData() {
+            for (long i = 0L; i < N; i++) {
+                memberRepository.save(new Member(UUID.randomUUID().toString(), "email" + (i + 1), "email" + (i + 1)));
+            }
+
+            for (long i = 0L; i < (N * N); i++) {
+                Member writer = memberRepository.findByEmail("email" + (i % N + 1)).orElseThrow();
+                postRepository.save(new Post(writer, Board.FREE, writer.getEmail(), "writer: " + writer.getName()));
+                postRepository.save(new Post(writer, Board.QUESTION, writer.getEmail(), "writer: " + writer.getName()));
+            }
+
+            for (long i = 0L; i < (N * N * N); i++) {
+                System.out.println("number: " + i % (N * N) + 1);
+                Member writer = memberRepository.findByEmail("email" + (i % N + 1)).orElseThrow();
+                Post post = postRepository.findById(i % (N * N) + 1).orElseThrow();
+                commentRepository.save(new Comment(writer, post, "writer: " + writer.getName() + " post: " + post.getId()));
+            }
+
+            for (long i = 0L; i < (N * N * N * N); i++) {
+                System.out.println("number: " + i % (N * N) + 1);
+                System.out.println("number: " + i % (N * N * N) + 1);
+                Member writer = memberRepository.findByEmail("email" + (i % N + 1)).orElseThrow();
+                Post post = postRepository.findById(i % (N * N) + 1).orElseThrow();
+                Comment comment = commentRepository.findById(i % (N * N * N) + 1).orElseThrow();
+                replyRepository.save(new Reply(writer, comment, "writer: " + writer.getName() + " post: " + post.getId() + " comment: " + comment.getId()));
+            }
+        }
+    }
+}
